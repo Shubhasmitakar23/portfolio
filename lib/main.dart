341893a4f84cd4cc61d8c43e462dcce3add2cf05
Shubhasmita Kar <103126498+Shubhasmitakar23@users.noreply.github.com>
2022-08-08 10:22:08 +0530

    add avatar

--- lib/main.dart
+++ lib/main.dart
@@ -26,6 +26,16 @@ class MyHomePage extends StatelessWidget {
       decoration: BoxDecoration(
         gradient: LinearGradient(begin: Alignment.topLeft, end: Alignment.bottomRight, colors: [Colors.green, Colors.blue])
       ),
+      child: Center(
+        child: Column(
+          mainAxisAlignment:MainAxisAlignment.center,
+          children: [
+          CircleAvatar(
+            radius: 70,
+            backgroundImage:  NetworkImage('https://media-exp1.licdn.com/dms/image/C4D03AQHQhgBsd-r85Q/profile-displayphoto-shrink_200_200/0/1653887873548?e=1665619200&v=beta&t=8pU6L4--XQ8qu6ZrgKThD_xpBOnwnAOM7T6AtHLM7B4'),
+          )
+        ],),
+      ),
     ));
   }
 }
